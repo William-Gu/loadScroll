4dc86e0ff891ad4129308de42396f1a0be0c8ca5
William Gu <gujungujun@qq.com>
2017-03-03 16:51:36 +0800

    over
    over

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # loadScroll.js 
 loadScroll是一个移动端的上拉、下拉加载更多的组件。基础库依赖jquery.js操作dom节点。
+
+###效果图:
+![demo1.png](./dist/imgs/eg1.png)
+![demo2.png](./dist/imgs/eg2.png)<br/> <br/>
+###demo地址: <a href="https://github.com/William-Gu/loadScroll">https://github.com/William-Gu/loadScroll/blob/master/demo/demo.html</a>
+
+
+###npm安装
+```javascript
+npm install loadScroll	//no now.
+```
+## 使用方法如下：
+
+###1、html结构
+```html
+<ul id="loadScroll">
+    <li>
+        <div>构建需要的代码</div>
+    </li>
+    <li data-del="true"><!-- 如果这一行需要删除的话 -->
+		<div class="loadScroll_cover">
+			<button data-id="ad">remove</button>
+		</div>
+		<div class="loadScroll_front">我是传奇</div>
+	</li>
+    <li>
+        <div>构建需要的代码</div>
+    </li>
+    <li>
+        <div>构建需要的代码</div>
+    </li>
+</ul>
+```
+1234566
+
+
+###2.1、需要引入的js
+```javascript
+<script src="../dist/loadScroll.js"></script>
+```
+
+###2.2、需要引入的css
+```javascript
+<script src="../dist/loadScroll.css"></script>
+```
+
+###3.1、创建参数
+```javascript
+// 创建loadScroll之前必须要先设置各参数
+var params={};
+params.topLoadingText=["下拉刷新","释放立即刷新","正在加载"],	//	[顶部原始提示,下拉提示,放开提示]
+params.refreshList=function(){console.log("refreshList")},	//列表刷新函数
+params.refreshListDone=function(data){console.log("refreshListDone")},
+params.refreshListError=function(data){console.log("refreshListError")},
+params.pushList={	//列表加载函数
+	url:"/a",
+	data:{},
+	success:function(){console.log("pishListDone")},
+	fail:function(){console.log("pishListFail")},
+	error:function(){console.log("pishListError")}
+},
+params.removeItem={	//ajax删除某项的调用
+	url:"/a",
+	data:{},
+	success:function(){console.log("removeItemDone")},
+	fail:function(){console.log("removeItemFail")},
+	error:function(){console.log("removeItemError")}
+}
+
+###3.2、调用loadScroll
+loadScroll(params);
+
+###4、API
+####4.1 服务器json 返回值
+```javascript
+<li><div class="loadScroll_front">我是传奇</div></li>
+```
+
+###目录结构
+loadScroll
+```javascript
+├────index.html         			// demo页面
+├────dist
+|      |──css
+|      |   └──loadScroll.css        // css
+|      └──js
+|          └──loadScroll.js         // js
+├────src
+|      |──scss
+|      |    └──loadScroll.scss     // 基础base样式
+```
--- dist/js/loadScroll.js
+++ dist/js/loadScroll.js
@@ -22,4 +22,184 @@ function loadScroll(params){
 	dom_span.innerText=params.topLoadingText[2];
     dom_bottomLoading.appendChild(dom_span);
 	$("body").append(dom_bottomLoading);
+
+
+//use fn with setTimeout in jQuery.
+jQuery.fn.extend({
+	timeOut:function(fn,time){
+		if ( typeof time !== "number" && fn == null ) {
+			return this;
+		}else{
+			var t=setTimeout(function(){
+				fn();
+				clearTimeout(t)
+			},time)
+		}
+	}
+});
+	var delMoveable=false,      //删除按钮可否移动
+		loading=false,       //是否在加载中
+		pageN = 1;              //要加载第几页
+		x=y=ex=ey=exx=eyy=0,  //位置和相对位置
+		scrollTop=0,            //距离顶部多少像素
+	if ( $(".loadScroll_cover").length ) { WDELETE=$(".loadScroll_cover button")[0].clientWidth }
+	var isLoading=false,
+		isEnd=false;
+
+	//对列表的操作
+	$("#loadScroll")
+		.on('touchstart',"li",function(e){
+			y=ey=eyy=scrollTop=0;
+			$(this).addClass('loadScroll_active');
+			y =  e.originalEvent.targetTouches[0].screenY;
+		})
+		.on('touchmove',"li",function(e){
+			ey=y-e.originalEvent.targetTouches[0].screenY;
+			scrollTop=$(window).scrollTop();
+			if(scrollTop==0 && ey<-5 ){
+				e.preventDefault();
+			}
+		})
+		.on('touchend',"li",function(e){
+			$(this).removeClass('loadScroll_active');
+		})
+		.on('touchstart',"[data-del=true] .loadScroll_front",function(e){
+			console.log("s")
+			x=ex=exx=0;
+			delMoveable=false;
+			x =  e.originalEvent.targetTouches[0].screenX;
+			if(!this.classList.contains("loadScroll_front_active")){
+				var $active=$(".loadScroll_front_active");
+				$(".loadScroll_front_active")
+					.css({"transition":"transform .2s","transform":"translateX(0)"})
+					.timeOut(function(){
+						$active.removeAttr("style").removeClass("loadScroll_front_active");
+					},200)
+			}
+		})
+		.on('touchmove',"[data-del=true] .loadScroll_front",function(e){
+			ex=x-e.originalEvent.targetTouches[0].screenX;
+			if(Math.abs(ex) > Math.abs(ey)){e.preventDefault();}
+			if(!this.classList.contains("loadScroll_front_active")){
+				exx=ex-10;
+				if(exx>0 && (Math.abs(exx) > Math.abs(ey*3) || delMoveable) ){
+					if(exx<=WDELETE){
+						$(this).css("transform","translateX("+exx*-1+"px)");
+						delMoveable=true;
+					}else{
+						$(this).css({"transition":"transform .2s","transform":"translateX("+WDELETE*-1+"px)"});
+					}
+				}
+			}
+		})
+		.on('touchend',"[data-del=true] .loadScroll_front",function(e){
+			if ((exx > WDELETE/3) && (Math.abs(exx) > Math.abs(ey*3))) {
+				$(this)
+					.css({"transition":"transform .2s","transform":"translateX("+WDELETE*-1+"px)"})
+					.addClass("loadScroll_front_active")
+					.timeOut(function(){
+						$(e.target).closest('.ui-article-front').css("transition","");
+					},200);
+			} else {
+				$(this)
+					.css({"transition":"transform .2s","transform":"translateX(0px)"})
+					.timeOut(function(){
+						$(e.target).closest('.ui-article-front').removeAttr("style").removeClass('loadScroll_front_active');
+					},200)
+			}
+		});
+
+	//对页面刷新的操作,有关参数已传播至页面
+	$(document)
+		.on('touchmove',function(e){
+			//顶部重新加载过程
+			eyy=ey*0.5+40;
+			if(scrollTop<=0  && eyy<0){
+				$('#loadScroll_topLoading,#loadScroll_bottomLoading, #loadScroll')
+					.css('transform',"translate3D(0,"+eyy*-1+"px,0)");
+				if(eyy >= -40){
+					$('#loadScroll_topLoading .loadScroll_loadingIcon').removeClass('icon-loading-animation')
+					$('#loadScroll_topLoading span').html(params.topLoadingText[0])
+				}else if(eyy < -40){
+					$('#loadScroll_topLoading .loadScroll_loadingIcon').addClass('icon-loading-animation')
+					$('#loadScroll_topLoading span').html(params.topLoadingText[1]);
+				}
+			}
+		})
+		.on('touchend',function(e){
+			//顶部重新加载
+			if(scrollTop<=0){
+				if(eyy < -40){
+					$("#loadScroll_topLoading,#loadScroll_bottomLoading, #loadScroll").css("transform","translate3D(0,"+dom_topLoading_height+"px,0)");
+					$('#loadScroll_loadingIcon').addClass('icon-loading-animation')
+					$("#loadScroll_topLoading span").html(params.topLoadingText[2]);
+					pushList();
+				}else{
+					returnBack();
+				}
+			}
+		})
+
+	$(window).scroll(function(){
+		$('.loadScroll_active').removeClass('loadScroll_active');
+		scrollTop = $(this).scrollTop();
+		if( !isEnd && !isLoading){
+			if(scrollTop + $(window).height() + 120 > $(document).height() ){
+				pushList();
+			}
+		}
+	})
+	$('.loadScroll_cover').on('click','button', function() {
+            //e.preventDefault();
+            var that=$(this);
+            var data=params.pushList.data;
+            	data.id=that.data().id;
+            $.ajax({
+                url:params.removeItem.url ||"",
+                data:data || {},
+                success:function(json){
+                    if(json.status==1){
+                        that.closest('li').slideUp('fast', function() {
+                        	params.removeItem.success() || ""
+                            if($('.ui-article').length==0){location.reload()}
+                        })
+                    }else if(json.status==2) {
+                        params.removeItem.fail() || ""
+                    }
+                },
+                error:function(json){
+                    params.pushList.error() || ""
+                }
+            })
+        })
+	function pushList(isfresh) {
+		isLoading=true;
+		$.ajax({
+			url:params.pushList.url || "",
+			data:params.pushList.data || {},
+			success:function(json){
+				if(json.status==1){
+					if(isfresh){
+						$("#loadScroll li").remove();
+					}
+					$("#loadScroll").append(json.data)
+					params.pushList.success() || "";
+				}else if(json.status==2){
+					params.pushList.fail() || "";
+				}
+			},
+			error:function(){
+				params.pushList.error() || "";
+			},
+			complete:function(){
+				isLoading=false;
+				returnBack()
+			}
+		})
+	}
+	function returnBack(){
+		$('#loadScroll_topLoading,#loadScroll_bottomLoading, #loadScroll').css("transform","translate3D(0,0,0)");
+		$('#loadScroll_loadingIcon').removeClass('icon-loading-animation')
+		$("#loadScroll_topLoading span").html(params.topLoadingText[0]);
+	}
 }
